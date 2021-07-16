7a17d8b748481b77c64294ef26f71f2775c362c9
Legitimoose <legitimoose@gmail.com>
2021-07-15 18:22:31 -0700

    Fixed regex match by using Pattern.compile!

--- src/main/kotlin/com/github/remynfv/emojitab/Emojifier.kt
+++ src/main/kotlin/com/github/remynfv/emojitab/Emojifier.kt
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextReplacementConfig
 import org.bukkit.configuration.file.FileConfiguration
 import org.jetbrains.annotations.NotNull
+import java.util.regex.Pattern
 
 class Emojifier(private val plugin: EmojiTab)
 {
@@ -33,8 +34,9 @@ class Emojifier(private val plugin: EmojiTab)
         var newMessage = message
         for (shortcode in emojiMap.keys)
         {
-            //Regex string was broken here, replaced with non regex version to get it working
-            val replacement: TextReplacementConfig = TextReplacementConfig.builder().matchLiteral("$shortcode").replacement(emojiMap.getValue(shortcode)).build()
+            //Create a Pattern to find and replace case insensitively
+            val regex: Pattern = Pattern.compile(shortcode, Pattern.LITERAL + Pattern.CASE_INSENSITIVE);
+            val replacement: TextReplacementConfig = TextReplacementConfig.builder().match(regex).replacement(emojiMap.getValue(shortcode)).build()
             newMessage = newMessage.replaceText(replacement)
         }
         return newMessage
@@ -75,7 +77,14 @@ class Emojifier(private val plugin: EmojiTab)
     private fun registerEmoji(character: String, shortcode: String)
     {
         val wrappingCharacter = plugin.wrappingCharacter
-        val shortcodeWithWrapping = wrappingCharacter + shortcode + wrappingCharacter
+        val maxLength = 16 - (2 * wrappingCharacter.length)
+
+        if (shortcode.length > maxLength)
+        {
+            Messager.warn("Emoji name '$wrappingCharacter$shortcode$wrappingCharacter' is over 16 characters and will be trimmed!")
+        }
+        val shortcodeCut = shortcode.take(maxLength)
+        val shortcodeWithWrapping = wrappingCharacter + shortcodeCut + wrappingCharacter
 
         if (emojiMap.containsKey(shortcodeWithWrapping))
         {
